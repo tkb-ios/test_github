e797b0cabac4fc0897fdb0fd2a88a635aa78138e
tucker <reioffitial@gmail.com>
2018-03-07 22:54:59 +0900

    ボタンを押すと文字列の表示が変化 MainActivity.javaを参照されたし。buttonのonClick()にメソッドを割り当てた。

--- app/src/main/java/com/tucker/test_create/MainActivity.java
+++ app/src/main/java/com/tucker/test_create/MainActivity.java
@@ -2,12 +2,27 @@ package com.tucker.test_create;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    public boolean change_flag = true;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void hello(View view){
+        TextView hellotext = findViewById(R.id.helloText);
+        if(change_flag){
+            change_flag = false;
+            hellotext.setText("Good Night...");
+        } else {
+            change_flag = true;
+            hellotext.setText("Hello World!!");
+        }
     }
 }
