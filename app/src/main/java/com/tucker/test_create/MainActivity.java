69f2ddc02b089f38d7ec953eb7412ab02fe329b1
Br3-kyokyo <danibusters.main@gmail.com>
2018-03-08 16:21:50 +0900

    練習としてコードを少し改変 ボタンを押したときの挙動を変更 コメントを追加

--- app/src/main/java/com/tucker/test_create/MainActivity.java
+++ app/src/main/java/com/tucker/test_create/MainActivity.java
@@ -7,22 +7,44 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    public boolean change_flag = true;
+    /***練習としてbooleanじゃなくてintでswitchするようにしてみました***/
+    /***あと、当該class内でしか使わないのでprivateにしてみた、どうかなあ。あと、あまり意味なさそうだけどstaticにもしておいた***/
+    //public boolean change_flag = true;
+    private static int flag = 0;
 
+    /***AppCompatActivity(大元はActivity)のonCreateメソッドのオーバーライド。よくわからない。***/
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
 
+    /***CHANGE!!ボタンが押されたときの挙動を記述するメソッド***/
     public void hello(View view){
         TextView hellotext = findViewById(R.id.helloText);
-        if(change_flag){
-            change_flag = false;
-            hellotext.setText("Good Night...");
-        } else {
-            change_flag = true;
-            hellotext.setText("Hello World!!");
+
+        switch (flag++){
+            case 0:
+                hellotext.setText("ONE!");
+                break;
+            case 1:
+                hellotext.setText("TWO!");
+                break;
+            case 2:
+                hellotext.setText("THREE!");
+                break;
+            case 3:
+                hellotext.setText("GOAL!");
+                flag = 0;
+                break;
         }
+
+//        if(change_flag){
+//            change_flag = false;
+//            hellotext.setText("Good Night...");
+//        } else {
+//            change_flag = true;
+//            hellotext.setText("Hello World!!");
+//        }
     }
 }
