5374b070aaa13a47f83b12670db9a33d2c5c7b2f
tucker <reioffitial@gmail.com>
2018-03-09 22:33:01 +0900

    Menu項目の編集
    レイアウトに関しては res/menu/main.xml を参照。レイアウトを反映させるため MainActivity.java に新規メソッドを2種追加。ハードコーディング部分を参照に修正。

--- README.md
+++ README.md
@@ -5,3 +5,9 @@ Android開発における試験運用のリポジトリになります。
 ### 各種環境
 - 作業環境：AndroidStudio 3.0.2
 - 使用言語：java
+
+
+### 変更履歴
+- 2018 / 03 / 09 <br><br>
+<span>　</span>ActionBar(画面右上)にMenuを追加。res/menu/main.xml を参照されたし。<br>
+<span>　</span>また、menuを表示をするために一部 MainActivity.java を変更。
--- app/src/main/java/com/tucker/test_create/MainActivity.java
+++ app/src/main/java/com/tucker/test_create/MainActivity.java
@@ -2,13 +2,16 @@ package com.tucker.test_create;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    /***練習としてbooleanじゃなくてintでswitchするようにしてみました***/
-    /***あと、当該class内でしか使わないのでprivateにしてみた、どうかなあ。あと、あまり意味なさそうだけどstaticにもしておいた***/
+    /***練習としてbooleanじゃなくてintでswitchするようにしてみました
+     ***あと、当該class内でしか使わないのでprivateにしてみた、どうかなあ。あと、あまり意味なさそうだけどstaticにもしておいた***/
     //public boolean change_flag = true;
     private static int flag = 0;
 
@@ -19,22 +22,56 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
+    /***右上ActionBarにMenuリソースを反映するために必要。詳しくはres/menu/main.xml参照。
+     ***メソッド内のコメントを解除すれば理解が深まるかと***/
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main, menu);
+        //inflater.inflate(R.menu.main, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    /***Menuで選択された項目に応じて処理を分ける部分。画面遷移処理を加工中***/
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.add_memo:
+                //add_memo(); メソッドを実行
+                return true;
+            case R.id.share_art:
+                //share_art(); メソッドを実行
+                return true;
+            case R.id.share_memo:
+                //share_memo(); メソッドを実行
+                return true;
+            case R.id.info:
+                //displayInfo(); メソッドを実行
+                return true;
+            case R.id.upload:
+                //upload_play(); メソッドを実行
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
     /***CHANGE!!ボタンが押されたときの挙動を記述するメソッド***/
     public void hello(View view){
         TextView hellotext = findViewById(R.id.helloText);
 
         switch (flag++){
             case 0:
-                hellotext.setText("ONE!");
+                hellotext.setText(R.string.one);
                 break;
             case 1:
-                hellotext.setText("TWO!");
+                hellotext.setText(R.string.two);
                 break;
             case 2:
-                hellotext.setText("THREE!");
+                hellotext.setText(R.string.three);
                 break;
             case 3:
-                hellotext.setText("GOAL!");
+                hellotext.setText(R.string.goal);
                 flag = 0;
                 break;
         }
