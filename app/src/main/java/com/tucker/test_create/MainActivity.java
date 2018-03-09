591ef49acb546c6493f9848c925ccb58829fe320
tucker <reioffitial@gmail.com>
2018-03-09 22:33:01 +0900

    Menu項目の編集
    レイアウトに関しては res/menu/main.xml を参照。レイアウトを反映させるため MainActivity.java に新規メソッドを2種追加。ハードコーディング部分を参照に修正。
    
    # Conflicts:
    #	.idea/misc.xml
    #	app/build.gradle
    #	app/src/main/java/com/tucker/test_create/MainActivity.java
    #	app/src/main/res/values/strings.xml

--- app/src/main/java/com/tucker/test_create/MainActivity.java
+++ app/src/main/java/com/tucker/test_create/MainActivity.java
@@ -2,6 +2,9 @@ package com.tucker.test_create;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.TextView;
 
@@ -13,6 +16,40 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    /***右上ActionBarにMenuリソースを反映するために必要。詳しくはres/menu/main.xml参照。
+     ***メソッド内のコメントを解除すれば理解が深まるかと***/
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main, menu);
+        //inflater.inflate(R.menu.main, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    /***Menuで選択された項目に応じて処理を分ける部分。画面遷移処理を加工中***/
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.add_memo:
+                //add_memo(); メソッドを実行
+                return true;
+            case R.id.share_art:
+                //share_art(); メソッドを実行
+                return true;
+            case R.id.share_memo:
+                //share_memo(); メソッドを実行
+                return true;
+            case R.id.info:
+                //displayInfo(); メソッドを実行
+                return true;
+            case R.id.upload:
+                //upload_play(); メソッドを実行
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 
     public void hello(View view){
