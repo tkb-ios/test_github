58cd2bb367903c5a9f18e7d287f2b9bc062bd2d9
tucker <reioffitial@gmail.com>
2018-03-13 12:02:02 +0900

    画面遷移機能の追加(遷移とレイアウトのみ)
    「画面遷移」のボタンを押すと画面を遷移する。遷移先はSubActivityであり、新しいメモの追加をするための画面となっている。画面遷移の挙動とレイアウトのみ追加した。
    
    # Conflicts:
    #	app/src/main/res/values/strings.xml

--- app/src/main/java/com/tucker/test_create/MainActivity.java
+++ app/src/main/java/com/tucker/test_create/MainActivity.java
@@ -1,11 +1,13 @@
 package com.tucker.test_create;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
@@ -16,6 +18,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button ActivityMover = findViewById(R.id.moveButton);
+        ActivityMover.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplication(), SubActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 
     /***右上ActionBarにMenuリソースを反映するために必要。詳しくはres/menu/main.xml参照。
