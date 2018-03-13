c466028217f612ce6b4c4d3a1372a21092164606
tucker <reioffitial@gmail.com>
2018-03-13 12:02:02 +0900

    画面遷移機能の追加(遷移とレイアウトのみ)
    「画面遷移」のボタンを押すと画面を遷移する。遷移先はSubActivityであり、新しいメモの追加をするための画面となっている。画面遷移の挙動とレイアウトのみ追加した。

---
+++ app/src/main/java/com/tucker/test_create/SubActivity.java
@@ -0,0 +1,23 @@
+package com.tucker.test_create;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class SubActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sub);
+
+        Button returnButton = findViewById(R.id.returnButton);
+        returnButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+    }
+}
