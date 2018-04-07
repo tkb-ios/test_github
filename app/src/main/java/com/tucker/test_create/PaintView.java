a085884a9f8d505b615d0aedd19134bce7a49d24
Unknown <s1611426@u.tsukuba.ac.jp>
2018-04-07 13:30:37 +0900

    描画するclassの追加
    daw_view.javaの追加

---
+++ app/src/main/java/com/tucker/test_create/PaintView.java
@@ -0,0 +1,62 @@
+package com.tucker.test_create;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.graphics.Path;
+import android.support.annotation.Nullable;
+import android.util.AttributeSet;
+import android.view.MotionEvent;
+import android.view.View;
+
+
+public class PaintView extends View {
+    private Paint paint;
+    private Path path;
+    public PaintView(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+        path=new Path();
+        paint=new Paint();
+        paint.setColor(0xFF000000);
+        paint.setStyle(Paint.Style.STROKE);
+        paint.setStrokeJoin(Paint.Join.ROUND);
+        paint.setStrokeCap(Paint.Cap.ROUND);
+        paint.setStrokeWidth(10);
+    }
+
+
+    public PaintView(Context context){
+        this(context,null);
+    }
+
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        canvas.drawPath(path, paint);
+    }
+
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event){
+        //描画処理
+        float x=event.getX();
+        float y=event.getY();
+
+        switch (event.getAction()) {
+            case MotionEvent.ACTION_DOWN:
+                path.moveTo(x, y);
+                invalidate();
+                break;
+            case MotionEvent.ACTION_MOVE:
+                path.lineTo(x, y);
+                invalidate();
+                break;
+            case MotionEvent.ACTION_UP:
+                path.lineTo(x, y);
+                invalidate();
+                break;
+        }
+        return true;
+
+    }
+}
